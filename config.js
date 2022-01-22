2a057aa26748f9bf4026338af08030475975f91d
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2022-01-22 15:30:53 +0100

    1.0.1
    - Remove server_name, server_ip, server_port, ip_copied
    - Added pinned_notice, pinned_text, pinned_text, pinned_read_more, pinned_url
    - Changed dark color from #24272b to  ##181818
    - Removed MC API

--- config.js
+++ config.js
@@ -3,12 +3,16 @@
  * License - https://olliejw.me/tos
  */
 
-var server_name = "YourServer";
-var server_ip = "hypixel.net";
-var server_port = 25565;
-var ip_copied = "IP copied!";
+
+var pinned_notice = true;
+var pinned_text = "Website under heavy developement!";
+var pinned_read_more = false;
+var pinned_url = "https://www.youtube.com/watch?v=XHx1ai1JWJ0";
+
 
 var firefly_count = 20;
+
+
 
 staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Owner");
 staff("OJWMine", "86fa328f-a99a-4a17-8250-5d333dd327fe", "Developer");
