9d4e33ab633d7d4b2f4844a975b3af8fd95f73a8
leo <townhelpemail@gmail.com>
2023-07-27 20:02:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,16 @@
 
 
 var pinned_notice = true;
-var pinned_text = "Dragon Race Season 5 Highlights out!";
+var pinned_text = "Join our event!";
 var pinned_read_more = true;
-var pinned_read_more_text = "Watch Here";
-var pinned_url = "https://youtu.be/GZGQQNcrIok";
+var pinned_read_more_text = "We are looking for some content creators to join our event and help Crownwars be popular! Learn more with the URL.";
+var pinned_url = "https://twitter.com/enjoyingsmain";
 
 
 
-staff("AsoDesu_", "Project Lead & Developer");
-staff("LukynkaCZE", "Lead Developer");
-staff("Ryeera", "Developer");
-staff("OnlyAdqm", "Builder & Editor");
-staff("CupcakeGaming882", "Builder");
-staff("claybutgray", "Sound Designer");
-staff("Swordington", "Testing Manager & Infrastructure");
+staff("enjoyings", "Project Lead & Developer");
+staff("PrismaticMC", "Lead Developer");
+staff("CCrven", "Developer");
+staff("HTNMC", "Builder & Editor");
+staff("Fur4knS", "Builder");
+staff("N/A", "Sound Designer");
